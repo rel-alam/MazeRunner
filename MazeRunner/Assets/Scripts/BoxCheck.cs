90f8ad6eb738d829c2fe4a81db58c2a1311683e1
Ronnie E <rel-alam@hotmail.com>
2015-03-11 16:26:17 +1100

    + Menu Scripts + Triggerable buttons + Movement

---
+++ MazeRunner/Assets/Scripts/BoxCheck.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxCheck : MonoBehaviour {
+
+    public GameObject button;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            print("Colliding with Collider");
+            if (Input.GetKeyUp("e"))
+            {
+                button.transform.position = new Vector3(0, 0, 0);
+                print("Boxed Moved!");
+                Destroy(this);
+            }
+        }
+    }
+}
---
+++ MazeRunner/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Rect button1;
+    public Rect button2;
+    public Texture texture1;
+    public Texture texture2;
+    
+	// Use this for initialization
+	void Start () {
+	
+        button1 = new Rect(Screen.width/2 - 100, Screen.height/2, 100, 30);
+        button2 = new Rect(Screen.width / 2 + 100, Screen.height / 2, 100, 30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(button1, texture1))
+        {
+            Application.LoadLevel("MainScene"); 
+        }
+        if (GUI.Button(button2, texture2))
+        {
+            Application.Quit();
+        }
+    }
+}
